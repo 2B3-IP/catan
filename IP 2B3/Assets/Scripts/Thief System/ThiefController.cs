33cdd679d2686c7a3f2b2d518b0b85c5d5329be4
Georgiana Matcovici <georgianamatcovici@gmail.com>
2025-04-19 15:20:40 +0300

    iteratia 2

---
+++ IP 2B3/Assets/Scripts/Thief System/ThiefController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using B3.GameStateSystem;
+using B3.PieceSystem;
+using B3.PlayerSystem;
+using B3.ResourcesSystem;
+using UnityEngine;
+
+namespace B3.ThiefSystem
+{
+    public abstract class ThiefController : MonoBehaviour
+    {
+        private PieceController _currentPiece;
+        
+        public abstract IEnumerator MoveThief(Vector3 endPosition);
+
+        public void BlockPiece(PieceController piece)
+        {
+            if(_currentPiece != null)
+                _currentPiece.IsBlocked = false;
+            
+            piece.IsBlocked = true;
+            _currentPiece = piece;
+        }
+
+        public void StealFromRandomPlayer(PlayerBase currentPlayer)
+        {
+            var settlements = _currentPiece.Settlements;
+            if (settlements.Count == 0)
+                return;
+            
+            int randomIndex = Random.Range(0, settlements.Count);
+            
+            var randomSettlement = settlements[randomIndex];
+            StealFromPlayer(randomSettlement.Owner, currentPlayer);
+        }
+
+        private void StealFromPlayer(PlayerBase stealFrom, PlayerBase giveTo)
+        {
+            var resources = stealFrom.Resources;
+            int index;
+            
+            // TODO: trb sa verifici daca stealFrom are macar o resursa > 0
+            
+            do
+            {
+                index = Random.Range(0, resources.Length);
+            } 
+            while (resources[index] == 0);
+            
+            var resource = (ResourceType)index;
+            
+            stealFrom.RemoveResource(resource, 1);
+            giveTo.AddResource(resource, 1);
+        }
+    }
+}

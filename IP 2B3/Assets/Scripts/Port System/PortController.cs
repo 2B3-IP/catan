50ae4b10bcf18064f424024b3fc8e776b06e8a7d
Lungu Fabian <fabian_lungu23@yahoo.com>
2025-04-26 14:15:27 +0300

    MovingPiece+PortPiece spawn part1

--- IP 2B3/Assets/Scripts/Port System/PortController.cs
+++ IP 2B3/Assets/Scripts/Port System/PortController.cs
@@ -1,11 +1,12 @@
 ﻿using B3.BuildingSystem;
 using B3.GameStateSystem;
+using B3.PieceSystem;
 using B3.PlayerBuffSystem;
 using UnityEngine;
 
 namespace B3.PortSystem
 {
-    public abstract class PortController : MonoBehaviour
+    public abstract class PortController : MovingPieceController
     {
         [SerializeField] private HousePivot[] portTransform;
         

228acf1159ab7b5a27866efd1e12e11365951a4e
andreeaplaton20 <platonandreea3@gmail.com>
2025-05-24 12:48:30 +0300

    updated thief

--- IP 2B3/Assets/Scripts/Thief System/ThiefController.cs
+++ IP 2B3/Assets/Scripts/Thief System/ThiefController.cs
@@ -5,9 +5,17 @@ namespace B3.ThiefSystem
 {
     internal sealed class ThiefController : ThiefControllerBase
     {
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float stoppingDistance = 0.01f;
         public override IEnumerator MoveThief(Vector3 endPosition)
         {
-            yield break;
+            while (Vector3.Distance(transform.position, endPosition) > stoppingDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPosition, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            transform.position = endPosition;
         }
     }
 }

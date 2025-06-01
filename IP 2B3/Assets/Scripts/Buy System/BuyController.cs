a097697371087f9c14feb559d4412b2b807c7f0d
RotDr <rotariudragos36@gmail.com>
2025-06-01 11:25:35 +0300

    warning notifications not enough resources

--- IP 2B3/Assets/Scripts/Buy System/BuyController.cs
+++ IP 2B3/Assets/Scripts/Buy System/BuyController.cs
@@ -6,7 +6,8 @@ using B3.GameStateSystem;
 using B3.PlayerInventorySystem;
 using B3.PlayerSystem;
 using UnityEngine;
-
+using UnityEngine.Playables;
+using B3.UI;
 namespace B3.BuySystem
 {
     internal sealed class BuyController : BuyControllerBase
@@ -16,8 +17,12 @@ namespace B3.BuySystem
         public override IEnumerator BuyHouse(PlayerBase player)
         {
             if (!HasEnoughResources(player, BuyItemType.House))
+            {
+                var warningNotif = NotificationManager.Instance
+                    .AddNotification("Player "+player.playerName+" doesn't have enough resources for a house", 5, false);
                 yield break;
-            
+            }
+
             Debug.Log("Buying house for: " + player.name);
             yield return buildingController.BuildHouse(player);
             
@@ -28,19 +33,29 @@ namespace B3.BuySystem
         public override IEnumerator BuyRoad(PlayerBase player)
         {
             if (!HasEnoughResources(player, BuyItemType.Road))
+            {
+                var warningNotif = NotificationManager.Instance
+                    .AddNotification("Player "+player.playerName+" doesn't have enough resources for a road", 5, false);
                 yield break;
-            
+            }
+
             yield return buildingController.BuildRoad(player);
-            
-            if(buildingController.HasBuilt)
+
+            if (buildingController.HasBuilt)
+            {
                 RemoveResources(player, BuyItemType.Road);
+            }
         }
 
         public override IEnumerator BuyCity(PlayerBase player)
         {
             if (!HasEnoughResources(player, BuyItemType.City))
+            {
+                var warningNotif = NotificationManager.Instance
+                    .AddNotification("Player "+player.playerName+" doesn't have enough resources for a city", 5, false);
                 yield break;
-            
+            }
+
             Debug.Log("Buying city for: " + player.name);
             yield return buildingController.BuildCity(player);
             
@@ -51,12 +66,20 @@ namespace B3.BuySystem
         public override DevelopmentCardType? BuyDevelopmentCard(PlayerBase player)
         {
             if (!HasEnoughResources(player, BuyItemType.DevelopmentCard))
+            {
+                var warningNotif = NotificationManager.Instance
+                    .AddNotification("Player "+player.playerName+" doesn't have enough resources for a dev card", 5, false);
                 return null;
-            
+            }
+
             var cardType = bankController.BuyDevelopmentCard();
-            if (cardType is null) 
+            if (cardType is null)
+            {
+                var warningNotif = NotificationManager.Instance
+                    .AddNotification("there aren't any dev cards in the bank", 5, false);
                 return null;
-            
+            }
+
             var inventory = player.GetComponent<PlayerInventoryController>();
             inventory.AddItem(cardType.Value);
             

7ebdcae2c2fb9056fc5a7c70cea41e362a8f3503
sebimaftei <sebastianmaftei2004@gmail.com>
2025-06-01 14:31:52 +0300

    disabling buttons when using a dev card

--- IP 2B3/Assets/Scripts/Development Card System/Cards/YearOfPlentyDevelopmentCard.cs
+++ IP 2B3/Assets/Scripts/Development Card System/Cards/YearOfPlentyDevelopmentCard.cs
@@ -8,19 +8,20 @@ using UnityEngine;
 namespace B3.DevelopmentCardSystem
 {
     [System.Serializable]
-    public sealed class YearOfPlentyDevelopmentCard : DevelopmentCardBase
+    public sealed class YearOfPlentyDevelopmentCard : DevelopmentCardBase   
     {
         [SerializeField] private BankController bankController;
         
-        public override IEnumerator UseCard(PlayerBase player)
+        public override IEnumerator UseCard(PlayerBase player, CanvasGroup actions)
         {
             ResourceType? selectedResourceType = null;
+            actions.interactable = false;
             yield return UISelectResource.SelectResourceType(resType => selectedResourceType = resType);
             Debug.Assert(selectedResourceType.HasValue); 
             
             bankController.GetResources(selectedResourceType.Value, 2);
             player.AddResource(selectedResourceType.Value, 2);
-            
+            actions.interactable = true;
         }
     }
 }
